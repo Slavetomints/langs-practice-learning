f077d32489c214631247b6fc2a4842a271f5a50b
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2025-01-01 18:26:46 -0600

    added the addItem, showList, and initToDoList functions

--- java/todo-list/src/toDoList.java
+++ java/todo-list/src/toDoList.java
@@ -1,11 +1,19 @@
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class toDoList {
+
+    // Initializing the file and the actual to do list to it can be referenced in multiple functions later. 
+    static ArrayList<String> toDoList = new ArrayList<String>();
+    static File toDoListFile = new File("tmp/to_do_list.txt");
     public static void main(String[] args) throws Exception {
-                        title();
-                        getMenuSelection(menu());
-                        goodbye();
+        title();
+        initToDoList();
+        getMenuSelection(menu());
+        goodbye();
     }
 
     public static void title() {
@@ -19,10 +27,26 @@ public class toDoList {
                         "\r\n" + //
                         "");
     }
+
+    public static void initToDoList() {
+        try {
+            Scanner reader = new Scanner(toDoListFile);
+            while (reader.hasNextLine()) {
+                String item = reader.nextLine();
+                toDoList.add(item);
+            }
+            reader.close();
+            System.out.println("To-Do List initialized");
+        } catch (IOException error) {
+            System.out.println("An error occurred");
+            error.printStackTrace();
+        }
+    }
+
     public static int menu() {
         // Initialize the scanner object that is going to be used to gather the menu input
-        // and the choice integer that will be used to return the menu selection. It is preinitialized to 3 so that 
-        // if for some reason it isnt changed all that happens is that the list is shown and no edits will be made to it
+        // and the choice integer that will be used to return the menu selection. It is initialized to 3 so that 
+        // if for some reason it isn't changed all that happens is that the list is shown and no edits will be made to it
         Scanner keyboard = new Scanner(System.in);
         int choice = 3;
 
@@ -41,10 +65,54 @@ public class toDoList {
     }
 
     public static void getMenuSelection(int selection) {
-        
+        switch(selection) {
+            case 1:
+                addItem();
+                menu();
+            case 2:
+                removeItem();
+                menu();
+            case 3:
+                showList();
+                menu();
+            case 999:
+                goodbye();
+            default:
+                System.out.println("Please make a valid selection");
+                menu();
+        }
+    }
+
+    public static void addItem() {
+        Scanner keyboard = new Scanner(System.in);
+        String item;
+
+        System.out.println("What do you want to add to the list?");
+        item = keyboard.nextLine();
+        toDoList.add(item);
+        keyboard.close();
+        showList();
+    }
+
+    public static void removeItem() {
+
+    }
+
+    public static void showList() {
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println(toDoList.get(i));
+          }
     }
 
     public static void goodbye() {
-
+        System.out.println("\r\n" + //
+                        "\r\n" + //
+                        " _____ _           _   _          _    _ _   _____     _ _        _ \r\n" + //
+                        "|_   _| |__   __ _| |_( )___     / \\  | | | |  ___|__ | | | _____| |\r\n" + //
+                        "  | | | '_ \\ / _` | __|// __|   / _ \\ | | | | |_ / _ \\| | |/ / __| |\r\n" + //
+                        "  | | | | | | (_| | |_  \\__ \\  / ___ \\| | | |  _| (_) | |   <\\__ \\_|\r\n" + //
+                        "  |_| |_| |_|\\__,_|\\__| |___/ /_/   \\_\\_|_| |_|  \\___/|_|_|\\_\\___(_)\r\n" + //
+                        "\r\n" + //
+                        "");
     }
 }
