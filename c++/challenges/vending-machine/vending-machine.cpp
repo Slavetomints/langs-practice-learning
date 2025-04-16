a17a7ca47dac496b3716f078352256b448141ac3
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2025-04-15 21:21:41 -0500

    cleaned up vending machine, its jank but it works

--- c++/challenges/vending-machine/vending-machine.cpp
+++ c++/challenges/vending-machine/vending-machine.cpp
@@ -12,8 +12,99 @@ double moneyAmount = 0;
 
 bool validateMoneyEntered(double money);
 bool validatePurchase(double money, double cost);
+void showMachine();
 
 int main() {
+  showMachine();
+
+  bool loopAgain = true;
+  vector<string> items;
+
+  while  (loopAgain) {
+    bool addMoney = true;
+    while (addMoney) {
+      double tempMoney;
+      string selection[1];
+      cout << "\nPlease enter an amount of money (accepts up to $20 bills, enter 0 to skip, enter 99 to stop)" << endl;
+      cout << "You currently have entered $" << fixed << setprecision(2) << moneyAmount << endl;
+      cin >> tempMoney;
+      if (tempMoney == 99) {
+        addMoney = false;
+      }
+      if (validateMoneyEntered(tempMoney)) {
+        moneyAmount += tempMoney;
+      } else {
+        cout << "\n\nPlease enter a valid denomination of currency (0.01¢, 0.05¢, 0.10¢, 0.25¢, 0.50¢, $1, $2, $5, $10, $20)" << endl;
+        continue;
+      }
+    }
+    bool getThings = true;
+    showMachine();
+    while (getThings) {
+      cout << "Please make a selection (ex. A1 for Cola), enter 99 to quit, enter 00 to see the vending machine again" << endl;
+      cout << "You currently have entered $" << fixed << setprecision(2) << moneyAmount << endl;
+      string inputSelection;
+      cin >> inputSelection;
+      if (inputSelection == "99") {
+        cout << "Your change is $" << fixed << setprecision(2) << moneyAmount << endl;;
+        cout << "You purchased:" << endl;
+        for (int i = 0; i < items.size(); i++) {
+          cout << "- " << items[i] << endl;
+        }
+        exit(0);
+      } else if (inputSelection == "00") {
+        showMachine();
+      }
+      vector<char> input(inputSelection.begin(), inputSelection.end());
+      if (input[0] == 'A' || input[0] == 'a') {
+        if (input[1] == '1') {
+          if (validatePurchase(moneyAmount, 2.25)) {
+            items.push_back("Cola");
+            moneyAmount -= 2.25;
+          } else {
+            cout << "Transaction Failed" << endl;
+          }
+        } else if (input[1] == '2') {
+            if (validatePurchase(moneyAmount, 50.00)) {
+              items.push_back("Pixie Dust");
+              moneyAmount -= 50.00;
+            } else {
+              cout << "Transaction Failed" << endl;
+            }
+        } else {
+          cout << "Invalid selection" << endl;
+        }
+      } else if (input[0] == 'B' || input[0] == 'b') {
+          if (input[1] == '1') {
+            items.push_back("Students tears");
+          } else if (input[1] == '2') {
+              if (validatePurchase(moneyAmount, 0.13)) {
+                items.push_back("Wi-Fi Password");
+                moneyAmount -= 0.13;
+              } else {
+                cout << "Transaction Failed" << endl;
+              }
+          } else {
+            cout << "Invalid selection" << endl;
+          }
+      } else {
+        cout << "Invalid selection" << endl;
+      }
+    }
+  }
+}
+
+bool validateMoneyEntered(double money) {
+  double validMoneyAmounts[] = {0, 0.01, 0.05, 0.10, 0.25, 0.50, 1, 2, 5, 10, 20};
+  auto valid = find(begin(validMoneyAmounts), end(validMoneyAmounts), money);
+  return (valid != end(validMoneyAmounts) );
+}
+
+bool validatePurchase(double money, double cost) {
+  return ((money - cost) > 0);
+}
+
+void showMachine() {
   cout << "_____________________________________________________" << endl;
   cout << "|                         |                          |" << endl;
   cout << "|                         |                          |" << endl;
@@ -48,73 +139,4 @@ int main() {
   cout << "|                                                    |" << endl;
   cout << "|                                                    |" << endl;
   cout << "_____________________________________________________" << endl;
-
-  bool loopAgain = true;
-  vector<string> items;
-
-  while  (loopAgain) {
-    double tempMoney;
-    string selection[1];
-    cout << "\nPlease enter an amount of money (accepts up to $20 bills, enter 0 to skip, enter 99 to stop)" << endl;
-    cout << "You currently have entered $" << fixed << setprecision(2) << moneyAmount << endl;
-    cin >> tempMoney;
-    if (validateMoneyEntered(tempMoney)) {
-      moneyAmount += tempMoney;
-    } else {
-      cout << "\n\nPlease enter a valid denomination of currency (0.01¢, 0.05¢, 0.10¢, 0.25¢, 0.50¢, $1, $2, $5, $10, $20)" << endl;
-      continue;
-    }
-    cout << "Please make a selection (ex. A1 for Cola)" << endl;
-    string inputSelection;
-    cin >> inputSelection;
-    vector<char> input(inputSelection.begin(), inputSelection.end());
-    if (input[0] == 'A') {
-      if (input[1] == '1') {
-        if (validatePurchase(moneyAmount, 2.25)) {
-          items.push_back("Cola");
-          moneyAmount -= 2.25;
-        } else {
-          cout << "Transaction Failed" << endl;
-        }
-      } else if (input[1] == '2') {
-          if (validatePurchase(moneyAmount, 50.00)) {
-            items.push_back("Pixie Dust");
-            moneyAmount -= 50.00;
-          } else {
-            cout << "Transaction Failed" << endl;
-          }
-      } else {
-        cout << "Invalid selection" << endl;
-      }
-    } else if (input[0] == 'B') {
-        if (input[1] == '1') {
-          items.push_back("Students tears");
-        } else if (input[1] == '2') {
-            if (validatePurchase(moneyAmount, 0.13)) {
-              items.push_back("Wi-Fi Password");
-              moneyAmount -= 0.13;
-            } else {
-              cout << "Transaction Failed" << endl;
-            }
-        } else {
-          cout << "Invalid selection" << endl;
-        }
-    } else {
-      cout << "Invalid selection" << endl;
-    }
-  }
-}
-
-bool validateMoneyEntered(double money) {
-  double validMoneyAmounts[] = {0, 0.01, 0.05, 0.10, 0.25, 0.50, 1, 2, 5, 10, 20};
-  if (money == 99) {
-    cout << "Your change is $" << fixed << setprecision(2) << moneyAmount; 
-    exit(0);
-  }
-  auto valid = find(begin(validMoneyAmounts), end(validMoneyAmounts), money);
-  return (valid != end(validMoneyAmounts) );
-}
-
-bool validatePurchase(double money, double cost) {
-  return ((money - cost) > 0);
 }
